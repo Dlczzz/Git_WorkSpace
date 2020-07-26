39ef620280255dff1ab3181af2dd579defc5ecdd
Dlczzz <1216172646@qq.com>
2020-07-26 15:27:22 +0800

    更新2

--- HelloGit/src/main/java/com/git/HelloGit.java
+++ HelloGit/src/main/java/com/git/HelloGit.java
@@ -1,12 +1,16 @@
 package com.git;
 
-/**
- * @Description
- * @Auther 段凌楚
- * @Date 2020-07-14 17:11
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        list.forEach(System.out::println);
     }
 }
